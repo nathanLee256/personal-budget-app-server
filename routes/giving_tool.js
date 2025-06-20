a4e593c2c610996a3ba8d521f401569c53b8f980
Nathan Lee <nathanleeahern@gmail.com>
2025-06-20 16:00:46 +1000

    added new server route for handling GET requests from GivingTool page. This route returns an array of DGR organisations to the client which populate autosuggest menu

--- routes/giving_tool.js
+++ routes/giving_tool.js
@@ -29,10 +29,54 @@ var router = express.Router();
         
 
 
-    })
+    });
 
 //END ROUTE 1
 
+//START ROUTE 2
+
+    /* 
+        route for handling GET requests from the useEffect in GivingTool.js which runs when the page renders, and requests
+        an object in the following form which contains an array of DGR organisations
+        data = {
+            "IsData": true,
+            "Organisations" : [
+                {"entityName": "Salvation Army", "abn": 1234567},
+                {"entityName": "Red Cross", "abn": 2345678}
+            ]
+        }
+     */
+    router.get("/retrieve_orgs", (req, res) => {
+        //prepare payload object
+        const payloadObj = {
+            IsData: true,
+            Organisations: []
+        };
+
+        //construct and execute the SQL query
+        req.db
+            .from("organisations")
+            .select(
+                "entity_name as entityName",
+                "organisation_abn as abn"
+            ) //select 2 columns and map them here to their respective properties in the payloadObj object
+            /* NB: This will give us exactly the structure our frontend expects, so no mapping required in the frontend */
+
+
+            /* this query should return an array of data objects (items) in the form above */
+            .then((items) => {
+                payloadObj.Organisations = items;
+                return res.json(payloadObj);
+                
+            })
+            .catch((error) => {
+                console.error("❌ Database error:", error.message, "\n", error.stack);
+                res.status(500).json({ error: "Internal server error", details: error.message });
+            });
+    });
+
+
+//END ROUTE 2
 
 
 // Export the router for use in other parts of the application
